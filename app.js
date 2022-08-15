18270e27f18712e8b481c725cb72874eda2bcdde
hussaini musa <hussainimusa566@gmail.com>
2022-08-15 09:08:56 -0700

    new commit

--- app.js
+++ app.js
@@ -46,6 +46,10 @@ app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static('./public'));
 app.use(fileUpload());
 
+app.get('/', (req, res) => {
+    res.send('e-commerce api')
+})
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
